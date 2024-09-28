ff157440cf59e98c085b8e5cc39563ace214ae0b
aniqlashari <mahaqamber3@gmail.com>
2024-09-28 18:39:30 +0500

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let name = prompt("enter your name");
+let name1 = prompt("enter your name");
 let name2 = prompt("Enter Your Father Name");
 let name3 = prompt("Enter Your School Name");
-let name4 = ptompt("Which Class You Read");
+let name4 = prompt("Which Class You Read");
 
-let Array = [name,name2,name3,name4];
-console.log(Array);
+let Aniq = [name1,name2,name3,name4];
+console.log(Aniq);
